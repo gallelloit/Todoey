cf92229468210428240c78d110c7a1711696008e
ElPiter <pgbonino@gmail.com>
2018-09-14 21:22:47 +0200

    Refactor application. Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Pedro Gallello Bonino on 14/9/18.
+//  Copyright © 2018 Pedro Gallello Bonino. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
